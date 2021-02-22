18f86eede6df9a104fb3b7dec193c1b9cb5cefe9
Sin24d <38560775+Sin24d@users.noreply.github.com>
2021-02-23 00:19:10 +0200

    interesting datasets

---
+++ model creation.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+delaney_with_descriptors_url = 'https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv'
+dataset = pd.read_csv(delaney_with_descriptors_url)
+dataset
+
+X = dataset.drop(['logS'], axis=1)
+X
+
+Y = dataset.iloc[:,-1]
+Y
+
+###Linear Regression Model
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+model = linear_model.LinearRegression()
+model.fit(X, Y)
+
+##Model Prediction
+Y_pred = model.predict(X)
+Y_pred
+
+##Model Preformance
+print('Coefficients:', model.coef_)
+print('Intercept:', model.intercept_)
+print('Mean squared error (MSE): %.2f'
+      % mean_squared_error(Y, Y_pred))
+print('Coefficient of determination (R^2): %.2f'
+      % r2_score(Y, Y_pred))
+
+##Model Equation
+print('LogS = %.2f %.2f LogP %.4f MW + %.4f RB %.2f AP' % (model.intercept_, model.coef_[0], model.coef_[1], model.coef_[2], model.coef_[3] ) )
+
+##Data Visualization
+import matplotlib.pyplot as plt
+import numpy as np
+plt.figure(figsize=(5,5))
+plt.scatter(x=Y, y=Y_pred, c="#7CAE00", alpha=0.3)
+# Add trendline
+# https://stackoverflow.com/questions/26447191/how-to-add-trendline-in-python-matplotlib-dot-scatter-graphs
+z = np.polyfit(Y, Y_pred, 1)
+p = np.poly1d(z)
+
+plt.plot(Y,p(Y),"#F8766D")
+plt.ylabel('Predicted LogS')
+plt.xlabel('Experimental LogS')
+
+##Save Model as Pickle Object
+import pickle
+pickle.dump(model, open('solubility_model.pkl', 'wb'))
